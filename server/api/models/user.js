d5c92c527501c5aa7b37e202518aad31f56e7919
sounak07 <sounakume@gmail.com>
2018-04-28 20:41:02 +0530

    Secret string changed

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -46,7 +46,7 @@ UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = "auth";
   var token = jwt
-    .sign({ _id: user._id.toHexString(), access }, "guess1234")
+    .sign({ _id: user._id.toHexString(), access }, process.env.SECRET)
     .toString();
 
   user.tokens.push({ access, token });
@@ -71,7 +71,7 @@ UserSchema.statics.findByToken = function(token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, "guess1234");
+    decoded = jwt.verify(token, process.env.SECRET);
   } catch (e) {
     return Promise.reject();
   }
